dfdededd2715af3bf6aa425e6c5eb931267e452c
sebastianjnuwu <sebastianjnuwu@gmail.com>
2025-10-04 16:35:42 -0300

    v1

--- front-end/src/components/jsx/ErudaClient.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import eruda from 'eruda';
-
-export default function ErudaClient() {
-  useEffect(() => {
-    if (import.meta.env.PUBLIC_ENV === 'development') eruda.init();
-  }, []);
-  return;
-}
